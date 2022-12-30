0f0464ee68962df2a7a977b4f5445f8dc5b1ea89
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2022-12-31 01:24:45 +0600

    customize file

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -3,6 +3,7 @@ import Featured from './HomeSections/Featured';
 import Hero from './HomeSections/Hero';
 import Destination from './HomeSections/Destination';
 
+
 const Homepage = () => {
     return (
         <div>
