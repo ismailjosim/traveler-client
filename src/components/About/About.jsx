9301aa9e5df8bcf5623be5fdee6a6a54f8488480
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-04 23:23:21 +0600

    add about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageHeading from '../../utilities/PageHeading';
+import Explore from './AboutSection/Explore';
 
 const About = () => {
     return (
         <div>
             <PageHeading headTitle={'about us'}></PageHeading>
+            <Explore />
         </div>
     );
 };
