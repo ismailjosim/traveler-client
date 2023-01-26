299edae3d06bccd726f60f70a3756f864095f2b8
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-27 01:20:27 +0600

    add single destination card and make hover

--- src/components/Destinations/Sections/AllDestinations.jsx
+++ src/components/Destinations/Sections/AllDestinations.jsx
@@ -1,38 +1,53 @@
-import React from 'react';
-import SectionHeading from '../../../utilities/SectionHeading';
+import React from 'react'
 import img01 from '../../../assets/destinations/destination1.jpg'
-import { Link } from 'react-router-dom';
+import '../../../styles/Custom.css'
+import SectionHeading from '../../../utilities/SectionHeading'
 
 const AllDestinations = () => {
-    const heading = {
-        subHeading: 'Top Destinations',
-        headingOne: 'Explore',
-        headingTwo: "Top Destinations",
-        describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-    }
+	const heading = {
+		subHeading: 'Top Destinations',
+		headingOne: 'Explore',
+		headingTwo: 'Top Destinations',
+		describe:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+	}
 
-    return (
-        <div className='mb-10'>
-            <SectionHeading heading={heading} />
-            <div className='grid lg:grid-cols-3'>
-                <div className="trend-item1">
-                    <div className="trend-image position-relative rounded">
-                        <img src={img01} alt="" />
-                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
-                            <div className="trend-content-title">
-                                <h5 className="mb-0">
-                                    <Link href="/" className="theme1">Italy</Link>
-                                </h5>
-                                <h3 className="mb-0 white">Caspian Valley</h3>
-                            </div>
-                            <span className="white bg-theme p-1 px-2 rounded">18 Tours</span>
-                        </div>
-                        <div className="color-overlay"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+	return (
+		<div className='mb-10'>
+			<SectionHeading heading={heading} />
+			<div className='w-11/12 mx-auto grid lg:grid-cols-3'>
+				<div class='destination_item'>
+					<div className='transition-all relative rounded-xl ease-in-out duration-500 overflow-hidden'>
+						<img
+							className='transition-all ease-in-out duration-500 flex w-full'
+							src={img01}
+							alt='image'
+						/>
+						<div class='trend-content flex items-center justify-between absolute bottom-0 p-6 w-full z-[1]'>
+							<div class='trend-content-title'>
+								<h5 class='mb-0'>
+									<a href='destination-detail.html' class='text-secondary'>
+										Italy
+									</a>
+								</h5>
+								<h3 class='mb-0 text-white text-2xl font-bold capitalize '>
+									Caspian Valley
+								</h3>
+							</div>
+							<span class='text-white bg-primary p-1 px-2 rounded-md'>
+								18 Tours
+							</span>
+						</div>
+						<div
+							class='color-overlay absolute left-0 bottom-0 h-1/2 w-full opacity-60 transition-all ease-in-out duration-500
+						bg-gradient-to-b from-transparent to-slate-900
+						'
+						></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default AllDestinations;
+export default AllDestinations
