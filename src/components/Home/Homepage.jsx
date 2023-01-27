0fc0fcbfa702e8861f8b46978d1ec62d2625f32a
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-27 17:11:56 +0600

    add top deals card and package card

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Featured from './HomeSections/Featured';
-import Hero from './HomeSections/Hero';
-import Destination from './HomeSections/Destination';
-import Activity from './HomeSections/Activity';
-import Partners from './HomeSections/Partners';
-
+import React from 'react'
+import Activity from './HomeSections/Activity'
+import Destination from './HomeSections/Destination'
+import Featured from './HomeSections/Featured'
+import Hero from './HomeSections/Hero'
+import Packages from './HomeSections/Packages'
+import Partners from './HomeSections/Partners'
+import TopDeals from './HomeSections/TopDeals'
 
 const Homepage = () => {
-    return (
-        <div>
-            <Hero />
-            <Featured />
-            <Destination />
-            <Activity />
-            <Partners />
-        </div>
-    );
-};
+	return (
+		<div>
+			<Hero />
+			<Featured />
+			<Destination />
+			<Packages />
+			<Activity />
+			<TopDeals />
+			<Partners />
+		</div>
+	)
+}
 
-export default Homepage;
+export default Homepage
