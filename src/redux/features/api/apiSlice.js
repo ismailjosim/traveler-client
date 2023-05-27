0b09d5999846e69c473ce4eedead91842cef35f4
ISMAIL <ismailjosim99@gmail.com>
2023-05-28 02:12:47 +0600

    add live server link

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000',
+        baseUrl: 'https://travel-server-woad.vercel.app',
     }),
     tagTypes: ["TourPackages"],
     endpoints: (builder) => ({
